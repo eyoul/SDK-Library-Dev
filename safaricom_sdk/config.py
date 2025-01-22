525f69cf59140f45156ba54473c22ca35847adb6
Eyoul S. <49184150+eyoul@users.noreply.github.com>
2025-01-22 04:00:21 -0800

    Update config.py

--- safaricom_sdk/config.py
+++ safaricom_sdk/config.py
@@ -4,22 +4,23 @@ from pydantic import BaseModel, HttpUrl
 class Configuration(BaseModel):
     """Configuration class for the Safaricom M-PESA SDK"""
     
-    consumer_key: str
-    consumer_secret: str
-    environment: str = "sandbox"  # or "production"
+    consumer_key: str = 'iYCMIkGY4jMlnXtVlybcZaZW7J2LybD11VGs6aI5MJbKuT8Q'  # Your Consumer Key
+    consumer_secret: str = 'Ld5Y9KJiLFNnGG5ceVNWxXp3tW1AYpZP7k6IG6kMQOpyEKOl43aKiYmjOxoPXglY'  # Your Consumer Secret
+    environment: str = "sandbox"  # Set to production
     timeout: int = 30
     max_retries: int = 3
-    
+    app_name: str = 'Testapp'  # Application name
+
     # API endpoints
-    base_url: HttpUrl = "https://apisandbox.safaricom.et"
-    auth_url: str = "/v1/token/generate"
+    base_url: HttpUrl = "https://developer.safaricom.et/apps"  # Ensure this is correct for production
+    auth_url: str = "/v1/oauth2/token"
     stkpush_url: str = "/mpesa/stkpush/v3/processrequest"
     b2c_url: str = "/mpesa/b2c/v1/paymentrequest"
     c2b_register_url: str = "/v1/c2b-registerurl/register"
     c2b_payment_url: str = "/v1/c2b/payments"
     
     # Optional configurations
-    api_key: Optional[str] = None
+    api_key: Optional[str] = None  # Update if you have an API key
     initiator_name: Optional[str] = None
     security_credential: Optional[str] = None
     
